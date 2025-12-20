a4f7217309bc5371a982d6d9b10a04cf68031a06
Luiz-Mendonca1 <luizeduardomendonca77@gmail.com>
2025-12-19 21:16:15 -0300

    add metadata site

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,21 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "dev-mechanic",
   description: "mecanica automotiva",
+  keywords:['oficina', 'oficina de carro', 'mecanico', 'manutenção de carros'],
+  openGraph:{
+    title: 'DevMechanic - Sua oficina ideal',
+    images: [`${process.env.NEXT_PUBLIC_URL}/logo.jpg`]
+  },
+  robots:{
+    index:true,
+    follow:true,
+    nocache:true,
+    googleBot:{
+      index:true,
+      follow:true,
+      noimageindex: true
+    }
+  }
 };
 
 export default function RootLayout({
