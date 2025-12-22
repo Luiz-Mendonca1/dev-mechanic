1434f38582c852e5c1bdde526d6e6edfed3ed707
Luiz-Mendonca1 <luizeduardomendonca77@gmail.com>
2025-12-21 23:59:51 -0300

    add dinamic metada

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -5,6 +5,47 @@ import { Hero } from '@/app/components/hero';
 import { Phone } from 'lucide-react';
 import { Container } from '@/app/components/container';
 import Image from 'next/image';
+import { Metadata } from 'next';
+
+export async function generateMetadata({params:{slug}}:{
+    params: {slug: string}
+}): Promise<Metadata> {
+    try{
+        const{objects}:PostProps= await getItemBySlug(slug)
+        .catch(()=>{
+            return{
+                title: "dev-mechanic",
+                description: "mecanica automotiva",
+            }
+        })
+        return{
+            title: `dev-mechanic - ${objects[0].title}`,
+            description: `${objects[0].metadata.description.text}`,
+            keywords:['devmechanic', 'troca de oleo'],
+            openGraph:{
+                title: `DevMechanic - ${objects[0].title}`,
+                images: [`${objects[0].metadata.banner.url}`]
+            },
+            robots:{
+                index:true,
+                follow:true,
+                nocache:true,
+                googleBot:{
+                index:true,
+                follow:true,
+                noimageindex: true
+    }
+  }
+        }
+
+    }catch(error){
+        return{
+            title: "dev-mechanic",
+            description: "mecanica automotiva",
+        }
+
+    }
+}
 
 export default async function Page({ params }: {
     params: Promise<{ slug: string }>
